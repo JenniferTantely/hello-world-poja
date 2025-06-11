ba21a361a26eb27341ceb943a88c73cc9877ee3b
Jenny <152529982+JenniferTantely@users.noreply.github.com>
2025-06-11 16:15:11 +0300

    feat: add reply by email in HelloWorldController.java

--- src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
@@ -1,7 +1,12 @@
 package com.example.demo.endpoint.rest.controller;
 
+import com.example.demo.mail.Email;
+import com.example.demo.mail.Mailer;
 import com.example.demo.service.HelloWorldService;
+import jakarta.mail.internet.InternetAddress;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class HelloWorldController {
   private final HelloWorldService service;
+  private final Mailer mailer;
+
+  @GetMapping("/helloo")
+  public String hellooWorld(@RequestParam String name) {
+    return service.uploadHelloWorldMessage(name);
+  }
 
   @GetMapping("/hello")
-  public String helloWorld(@RequestParam String name) {
-    return service.uploadHelloWorldMessage(name);
+  @SneakyThrows
+  public String helloWorld(@RequestParam String to) {
+    var email =
+        new Email(
+            new InternetAddress(to), List.of(), List.of(), "Hello world", "... world!", List.of());
+
+    mailer.accept(email);
+    return "... world!";
   }
 }
